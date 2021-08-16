e8a72868c7758ebe5ec9480d5ebb85a6b8acf3bf
Messquerade <annac.klingberg@gmail.com>
2021-08-16 14:04:16 -0700

    add loop to insert inputs in text

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,13 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
-    const person1Input = $("input#person1").val();
-    const person2Input = $("input#person2").val();
-    const nounInput= $("input#noun").val();
-    const exclamationInput = $("input#exclamation").val();
-    const verbInput = $("input#verb").val();
-    const actionInput = $("input#action").val();
+    const inputs = [$("input#person1").val(), $("input#person2").val(), $("input#noun").val(), $("input#exclamation").val(), $("input#verb").val(), $("input#action").val()];
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".noun").text(nounInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".action").text(actionInput);
+    i = 1
+    inputs.forEach(function(input) {
+      $(".span" + i.toString()).text(input);
+      i ++
+    });
 
     $("#story").show();
   });
